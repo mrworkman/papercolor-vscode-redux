310b74aaf23db5b2d53b6478077abdec8638c2c6
Stephen Workman <workman.stephen@gmail.com>
2019-02-12 15:05:41 -0500

    Initial commit.

---
+++ demo/cs.cs
@@ -0,0 +1,30 @@
+public class SomeClass {
+   
+   private int _someint = 22;
+   private const double x = 0.0;
+   private readonly object o = new Object();
+   
+   // Some Comment...
+
+   public SomeClass(float fl) {
+      int l = 55;
+      string x = "Some String";
+      foo();
+   }
+
+   public static void foo() {
+      var q = (a) => {
+         var verbatim = @"verbatim";
+      };
+
+      if (1 == 1) {
+         return;
+      } else {
+         throw new Exception("Some Exception");
+      }
+   }
+
+   [SomeAnnotation(Something = "Something")]
+   public string Something { get; set; }
+
+}
